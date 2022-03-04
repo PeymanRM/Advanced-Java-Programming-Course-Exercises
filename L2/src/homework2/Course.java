8d7355a638fe8901048f842e91c408895bee3a1d
Peyman RM <PRM.8193@gmail.com>
2022-03-04 17:25:12 +0330

    homework2 added

---
+++ L2/src/homework2/Course.java
@@ -0,0 +1,54 @@
+package homework2;
+
+public class Course {
+    private int coefficient;
+    private String courseName;
+    private Course[] prerequisites, corequisites;
+
+    Course(String courseName, int coefficient, int preCount, int coCount){
+        this.courseName = courseName;
+        this.coefficient = coefficient;
+        if(preCount != 0) prerequisites = new Course[preCount];
+        if(coCount != 0) corequisites = new Course[coCount];
+    }
+
+    public void addPrerequisite(Course prerequisite) {
+        if (prerequisites != null) {
+            for (int i = 0; i < prerequisites.length; i++) {
+                if (prerequisites[i] == null) {
+                    prerequisites[i] = prerequisite;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void addCorequisite(Course corequisite) {
+        if (corequisites != null) {
+            for (int i = 0; i < corequisites.length; i++) {
+                if (corequisites[i] == null) {
+                    corequisites[i] = corequisite;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void getCourseInfo(){
+        System.out.println("Course Name: " + courseName + "\t\tCoefficient: " + coefficient);
+        if(prerequisites != null){
+            System.out.println("Prerequisites: ");
+            for (Course prerequisite : prerequisites) {
+                System.out.println("\t" + prerequisite.courseName);
+            }
+        }else System.out.println("No prerequisites");
+
+        if(corequisites != null){
+            System.out.println("Corequisites: ");
+            for (Course corequisite : corequisites) {
+                System.out.println("\t" + corequisite.courseName);
+            }
+        }else System.out.println("No corequisites");
+        System.out.println();
+    }
+}
---
+++ L2/src/homework2/Main.java
@@ -0,0 +1,20 @@
+package homework2;
+
+public class Main {
+    public static void main(String[] args) {
+        Course[] prerequisites, corequisites;
+        Course c1 = new Course("ریاضی 1",3,0,0);
+        Course c2 = new Course("ریاضی 2",3,1,0);
+        c2.addPrerequisite(c1);
+        Course c3 = new Course("مبانی کامپیوتر",3,0,0);
+        Course c4 = new Course("ریاضیات گسسته",3,0,2);
+        c4.addCorequisite(c1);
+        c4.addCorequisite(c3);
+
+        c1.getCourseInfo();
+        c2.getCourseInfo();
+        c3.getCourseInfo();
+        c4.getCourseInfo();
+
+    }
+}
