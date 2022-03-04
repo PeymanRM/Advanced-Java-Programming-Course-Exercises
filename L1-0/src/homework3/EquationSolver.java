1db9b9c2e6ca1a19a953cee3940bc14b35f02ffb
Peyman RM <PRM.8193@gmail.com>
2022-03-04 13:22:57 +0330

    L1

---
+++ L1-0/src/homework3/EquationSolver.java
@@ -0,0 +1,25 @@
+package homework3;
+
+public class EquationSolver {
+    private double a,b,c,d,e,f;
+
+    public void setEquation1 (double a, double b, double e){
+        this.a = a;
+        this.b = b;
+        this.e = e;
+    }
+
+    public void setEquation2 (double c, double d, double f){
+        this.c = c;
+        this.d = d;
+        this.f = f;
+    }
+
+    public void getSolution() {
+        if(a*d-b*c == 0){
+            System.out.println("Cannot Solve");
+        } else {
+            System.out.println("x=" + ((e*d-b*f)/(a*d-b*c)) + "\t\ty=" + ((a*f-e*c)/(a*d-b*c)));
+        }
+    }
+}
