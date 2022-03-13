93c9547e7a55b00cba010f320b6eadf20e767eac
Peyman RM <PRM.8193@gmail.com>
2022-03-13 13:37:13 +0330

    created NumberException

---
+++ L3/src/homework3/NumberException.java
@@ -0,0 +1,12 @@
+package homework3;
+
+/**
+ * Created by Peyman RM
+ */
+public class NumberException extends Exception {
+    public NumberException(String msg) {
+        super(msg);
+    }
+
+    public NumberException() {}
+}
