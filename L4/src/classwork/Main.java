ca8454d220825349d4edf8dcc7e44ba179ad1a7c
Peyman RM <PRM.8193@gmail.com>
2022-03-19 19:30:19 +0330

    classwork completed

--- L4/src/classwork/Main.java
+++ L4/src/classwork/Main.java
@@ -5,6 +5,12 @@ package classwork;
  */
 public class Main {
     public static void main(String[] args) {
-
+        User user = new User("PeymanRM", "JHvU6KHb", "Peyman",
+                "Roohi-Moghadam", "Back-End Developer", 19);
+        System.out.println("name: " + user.getName());
+        System.out.println("family: " + user.getFamily());
+        System.out.println("username: " + user.getUsername());
+        System.out.println("job: " + user.getJob());
+        System.out.println("age: " + user.getAge());
     }
 }
