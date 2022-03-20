e579501fd8121c19bdf64d461bb5b876de4f0734
Peyman RM <PRM.8193@gmail.com>
2022-03-20 14:23:52 +0330

    setters implemented

---
+++ L4/src/homework3/Student.java
@@ -0,0 +1,28 @@
+package homework3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Peyman RM
+ */
+public class Student <userdata> {
+    private userdata studentCode, name, fatherName;
+    private List<Double> grades = new ArrayList<>();
+
+    public void setName(userdata name) {
+        this.name = name;
+    }
+
+    public void setStudentCode(userdata studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public void setFatherName(userdata fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public void setGrades(List<Double> grades) {
+        this.grades = grades;
+    }
+}
