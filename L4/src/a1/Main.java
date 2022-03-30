3a32aa5231248e64aab6b84c5bc34e6481cc7fab
Peyman RM <PRM.8193@gmail.com>
2022-03-31 03:29:04 +0430

    a1 completed

--- L4/src/a1/Main.java
+++ L4/src/a1/Main.java
@@ -12,10 +12,24 @@ public class Main {
         Set <String> names = new TreeSet<>();
         System.out.println("Enter a set of names with a space in between:");
         Scanner input = new Scanner(System.in);
-        for (String name : input.nextLine().trim().split(" ")) {
+        for (String name : input.nextLine().trim().toLowerCase().split(" ")) {
             names.add(name);
         }
 
+        while (true) {
+            System.out.println("\nEnter the name you want to search:");
+            String searchedName = input.next().toLowerCase();
+            boolean foundName = false;
+            for (String name : names) {
+                if(name.equals(searchedName)){
+                    foundName = true;
+                    break;
+                }
 
+            }
+
+            if(foundName) System.out.println("Name Found");
+            else System.out.println("Name Not Found");
+        }
     }
 }
