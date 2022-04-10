3e6f385c6b23380955a183af4a1a09c0d210e945
Peyman RM <PRM.8193@gmail.com>
2022-04-10 13:22:42 +0430

    completed Database

--- L5/src/homework2/Database.java
+++ L5/src/homework2/Database.java
@@ -12,4 +12,22 @@ public class Database {
         return database;
     }
     private Database() {}
+
+    private String username, password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
