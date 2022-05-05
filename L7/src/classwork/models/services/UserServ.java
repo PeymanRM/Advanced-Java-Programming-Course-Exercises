ad4d224eb284ea382a4ae6609692f7e653ee1c59
Peyman RM <PRM.8193@gmail.com>
2022-05-05 18:09:38 +0430

    UserServ implemented

--- L7/src/classwork/models/services/UserServ.java
+++ L7/src/classwork/models/services/UserServ.java
@@ -1,5 +1,10 @@
 package classwork.models.services;
 
+import classwork.models.entities.UserEnti;
+import classwork.models.repositories.UserRepo;
+
+import java.util.List;
+
 public class UserServ {
     private UserServ() {}
     private UserServ userServ = new UserServ();
@@ -7,5 +12,19 @@ public class UserServ {
         return userServ;
     }
 
+    public void save(UserEnti user) throws Exception {
+        UserRepo userRepo = new UserRepo();
+        userRepo.insertUser(user);
+        userRepo.commit();
+    }
 
+    public UserEnti getUser(String username) throws Exception{
+        UserRepo userRepo = new UserRepo();
+        return userRepo.selectUser(username);
+    }
+
+    public boolean verifyUser(String username, String enteredPassword) throws Exception{
+        UserRepo userRepo = new UserRepo();
+        return userRepo.selectUserPassword(username).equals(enteredPassword);
+    }
 }
