7d2381d5a1c670d2399c7fa7ec954f2394a35750
Peyman RM <PRM.8193@gmail.com>
2022-05-11 23:37:51 +0430

    homework2 initial commit + added edit button to dashboard-view.fxml

---
+++ L8/src/homework2/controllers/EditMenuController.java
@@ -0,0 +1,4 @@
+package homework2.controllers;
+
+public class EditMenuController {
+}
---
+++ L8/src/homework2/models/services/UserServ.java
@@ -0,0 +1,32 @@
+package homework2.models.services;
+
+import homework2.models.entities.UserEnti;
+import homework2.models.repositories.UserRepo;
+
+/**
+ * Created by Peyman RM
+ */
+public class UserServ {
+    private UserServ() {}
+    private static UserServ userServ = new UserServ();
+    public static UserServ getInstance(){
+        return userServ;
+    }
+
+    public void save(UserEnti user) throws Exception {
+        UserRepo userRepo = new UserRepo();
+        userRepo.insertUser(user);
+        userRepo.commit();
+        userRepo.close();
+    }
+
+    public UserEnti getUser(String username) throws Exception{
+        UserRepo userRepo = new UserRepo();
+        return userRepo.selectUser(username);
+    }
+
+    public boolean verifyUser(String username, String enteredPassword) throws Exception{
+        UserRepo userRepo = new UserRepo();
+        return userRepo.selectUserPassword(username).equals(enteredPassword);
+    }
+}
